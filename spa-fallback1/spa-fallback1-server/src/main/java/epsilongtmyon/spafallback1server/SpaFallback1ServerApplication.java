0b7c9e21e0e76511239c648d984ebf7ddfe83e82
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-01-17 21:38:14 +0900

    add server side

---
+++ spa-fallback1/spa-fallback1-server/src/main/java/epsilongtmyon/spafallback1server/SpaFallback1ServerApplication.java
@@ -0,0 +1,13 @@
+package epsilongtmyon.spafallback1server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaFallback1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaFallback1ServerApplication.class, args);
+	}
+
+}
---
+++ spa-fallback1/spa-fallback1-server/src/test/java/epsilongtmyon/spafallback1server/SpaFallback1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package epsilongtmyon.spafallback1server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaFallback1ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
