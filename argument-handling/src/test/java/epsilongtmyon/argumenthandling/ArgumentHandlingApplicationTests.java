d74faa3524db993a71b779f31edaa3cb8f1950a6
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-01-25 22:06:43 +0900

    add project

---
+++ argument-handling/src/test/java/epsilongtmyon/argumenthandling/ArgumentHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package epsilongtmyon.argumenthandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgumentHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
