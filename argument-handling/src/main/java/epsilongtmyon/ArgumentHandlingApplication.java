6275fc8d2f0b529748fd1335fafacf03d76735d4
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-01-27 22:49:37 +0900

    よく使うパラメータを使っておっていろいろメモ

--- argument-handling/src/main/java/epsilongtmyon/ArgumentHandlingApplication.java
+++ argument-handling/src/main/java/epsilongtmyon/ArgumentHandlingApplication.java
@@ -1,4 +1,4 @@
-package epsilongtmyon.argumenthandling;
+package epsilongtmyon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- argument-handling/src/test/java/epsilongtmyon/ArgumentHandlingApplicationTests.java
+++ argument-handling/src/test/java/epsilongtmyon/ArgumentHandlingApplicationTests.java
@@ -1,4 +1,4 @@
-package epsilongtmyon.argumenthandling;
+package epsilongtmyon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
