0a43720b69fa98a2011cfa54c2e32a26951d879b
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-02-15 22:08:57 +0900

    add new project http-message-converter

---
+++ http-message-converter/src/main/java/epsilongtmyon/httpmessageconverter/HttpMessageConverterApplication.java
@@ -0,0 +1,13 @@
+package epsilongtmyon.httpmessageconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpMessageConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpMessageConverterApplication.class, args);
+	}
+
+}
---
+++ http-message-converter/src/test/java/epsilongtmyon/httpmessageconverter/HttpMessageConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package epsilongtmyon.httpmessageconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpMessageConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
