ea23561f51ee804e7c17add3183480f0b27fa40c
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-09-28 00:16:39 +0900

    準備中

---
+++ ajax-json-authentication/src/main/java/epsilongtmyon/AjaxJsonAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package epsilongtmyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxJsonAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxJsonAuthenticationApplication.class, args);
+	}
+
+}
