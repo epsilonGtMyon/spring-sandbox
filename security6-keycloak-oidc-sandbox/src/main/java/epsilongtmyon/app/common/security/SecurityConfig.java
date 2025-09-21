d10708233310fb97d4427470991e9b9f3681e0c3
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2025-09-22 00:53:07 +0900

    OidcUserService の設定

--- security6-keycloak-oidc-sandbox/src/main/java/epsilongtmyon/app/common/security/SecurityConfig.java
+++ security6-keycloak-oidc-sandbox/src/main/java/epsilongtmyon/app/common/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package epsilongtmyon.app.common.security;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Bean;
@@ -7,10 +10,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -44,6 +58,11 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  * Principalの部分を作ってるのは OidcUserService
  * 内部にはDefaultOAuth2UserServiceとかがある。
  * あとはOidcUserService#setOidcUserMapperあたりが変換カスタマイズできそうかも
+ * 
+ * 
+ * 
+ * 参考
+ * ・OAuth2LoginConfigurer#init
  */
 
 @Configuration
@@ -75,19 +94,20 @@ public class SecurityConfig {
 					// これがないと遷移しようとしたページに?continueという感じになる。
 					c.defaultSuccessUrl("/home", true);
 
-					c.authorizationEndpoint(ace -> {
+					c.authorizationEndpoint(aec -> {
 						// OAuth2AuthorizationRequestRedirectFilter の挙動設定
 
 						// OAuth2LoginConfigurer#getAuthorizationRequestResolver() を参考
 						String authorizationRequestBaseUri = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
 						DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver = new DefaultOAuth2AuthorizationRequestResolver(
 								clientRegistrationRepository, authorizationRequestBaseUri);
-						
+
 						// カスタマイザーをセットしてIdPへのリクエスト時にクエリパラメータ追加
 						// これがOIDCの仕様として許容されるのかは別の話
 						authorizationRequestResolver.setAuthorizationRequestCustomizer(b -> {
-							HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
-							
+							HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+									.getRequestAttributes()).getRequest();
+
 							b.additionalParameters(params -> {
 								// 固定値
 								params.put("myParam1", "hello");
@@ -95,7 +115,17 @@ public class SecurityConfig {
 								params.put("myParam2", request.getHeader("host"));
 							});
 						});
-						ace.authorizationRequestResolver(authorizationRequestResolver);
+						aec.authorizationRequestResolver(authorizationRequestResolver);
+					});
+
+					c.userInfoEndpoint(uiec -> {
+
+						// OidcAuthorizationCodeAuthenticationProvider の挙動設定
+
+						// ユーザー情報のマッピングまわりに関係するところ
+						OidcUserService oidcUserService = new OidcUserService();
+						oidcUserService.setOidcUserMapper(SecurityConfig::getUser);
+						uiec.oidcUserService(oidcUserService);
 					});
 
 				});
@@ -115,5 +145,25 @@ public class SecurityConfig {
 					r.anyRequest().authenticated();
 				});
 		return http.build();
+	}
+
+	// OidcUserService.oidcUserMapper フィールドの初期値である OidcUserRequestUtils#getUserを参考
+	private static OidcUser getUser(OidcUserRequest userRequest, OidcUserInfo userInfo) {
+		Set<GrantedAuthority> authorities = new LinkedHashSet<>();
+		ClientRegistration.ProviderDetails providerDetails = userRequest.getClientRegistration().getProviderDetails();
+		String userNameAttributeName = providerDetails.getUserInfoEndpoint().getUserNameAttributeName();
+		if (StringUtils.hasText(userNameAttributeName)) {
+			authorities.add(new OidcUserAuthority(userRequest.getIdToken(), userInfo, userNameAttributeName));
+		} else {
+			authorities.add(new OidcUserAuthority(userRequest.getIdToken(), userInfo));
+		}
+		OAuth2AccessToken token = userRequest.getAccessToken();
+		for (String scope : token.getScopes()) {
+			authorities.add(new SimpleGrantedAuthority("SCOPE_" + scope));
+		}
+		if (StringUtils.hasText(userNameAttributeName)) {
+			return new DefaultOidcUser(authorities, userRequest.getIdToken(), userInfo, userNameAttributeName);
+		}
+		return new DefaultOidcUser(authorities, userRequest.getIdToken(), userInfo);
 	}
 }
